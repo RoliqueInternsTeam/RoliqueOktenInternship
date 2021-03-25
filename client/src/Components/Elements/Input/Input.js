a888cafe2091d2500cc0df33a125a6753e569081
Andrii <andriikosanyak4@gmail.com>
2021-03-25 17:41:36 +0200

    eslint added

--- client/src/Components/Elements/Input/Input.js
+++ client/src/Components/Elements/Input/Input.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import classes from './Input.module.css';
 
+const Input = (props) =>
+// let divClass = [];
+// if (props.className)
+//     {divClass.push(props.className)}
 
-const Input = (props) => {
-    // let divClass = [];
-    // if (props.className)
-    //     {divClass.push(props.className)}
-
-    return (
-        <div className={`${classes.div} ${classes[props.className]}`}>
-            <label className={classes.label} for={props.id}>{props.label}</label>
-            <input className={classes.input} type={props.type} id={props.id}/>
-            {props.error ? <p className={classes.p}>{props.error}</p> : null}
-        </div>
-    );
-};
-
+  (
+    <div className={`${classes.div} ${classes[props.className]}`}>
+      <label className={classes.label} htmlFor={props.id}>{props.label}</label>
+      <input className={classes.input} type={props.type} id={props.id} />
+      {props.error ? <p className={classes.p}>{props.error}</p> : null}
+    </div>
+  );
 export default Input;
--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 import classes from './Sidebar.module.css';
-import Vector from '../Elements/Icons/vector.svg'
-import Users from '../Elements/Icons/users.svg'
-import Volume from '../Elements/Icons/volume.svg'
-import At from '../Elements/Icons/at.svg'
+import Vector from '../Elements/Icons/vector.svg';
+import Users from '../Elements/Icons/users.svg';
+import Volume from '../Elements/Icons/volume.svg';
+import At from '../Elements/Icons/at.svg';
 
-const Sidebar = (props) => {
-    return (
-        <div className={classes.sidebar}>
-            <div className={classes.vectorContainer}>
-            <img src={Vector} alt='vector icon'/>
-            </div>
-            <div className={classes.imgContainer}  onClick={() => props.history.push('/create')}>
-            <img src={Users} alt='create user'/>
-            </div>
-            <div className={classes.imgContainer}  onClick={() => props.history.push('/campaigns')}>
-            <img src={Volume} alt='campaigns'/>
-            </div>
-            <div className={classes.imgContainer} onClick={() => props.history.push('/users')}>
-            <img src={At} alt='users'/>
-            </div>
-        </div>
-    );
-};
+const Sidebar = (props) => (
+  <div className={classes.sidebar}>
+    <div className={classes.vectorContainer}>
+      <img src={Vector} alt="vector icon" />
+    </div>
+    <div className={classes.imgContainer} onClick={() => props.history.push('/create')}>
+      <img src={Users} alt="create user" />
+    </div>
+    <div className={classes.imgContainer} onClick={() => props.history.push('/campaigns')}>
+      <img src={Volume} alt="campaigns" />
+    </div>
+    <div className={classes.imgContainer} onClick={() => props.history.push('/users')}>
+      <img src={At} alt="users" />
+    </div>
+  </div>
+);
 
 export default withRouter(Sidebar);
