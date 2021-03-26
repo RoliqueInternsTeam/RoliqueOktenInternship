fc9061697b7e2906250a6fa0d059908effc2d841
0utfire <nataniel.rr@gmail.com>
2021-03-26 14:13:30 +0200

    Login form styles completed, styles fixed

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -4,14 +4,15 @@ import classes from './Login.module.css';
 // import Message from '../Elements/Message/Message';
 
 const Login = () => (
-  <div className={classes.loginForm}>
+  <form className={classes.loginForm}>
     <h3 className={classes.h3}>Log into your account</h3>
+    {/* <Message style={['error-bg-color', 'error-icon-color', 'error-text-color']} message={props.message} /> */}
     <div className={classes.loginContainer}>
-      <Input className="loginInput" id="email" label="Email" type="email" />
-      <Input className="loginInput" id="password" label="Password" type="password" />
+      <Input className="loginInput" id="email" label="Email" type="email" required='required' />
+      <Input className="loginInput" id="password" label="Password" type="password" required='required' />
     </div>
     <button type="submit" className={classes.button}>Log In</button>
-  </div>
+  </form>
 );
 
 export default Login;
