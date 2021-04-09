18946ac75ee7c0768fab9974c5096b6077b446b6
Andrii <andriikosanyak4@gmail.com>
2021-04-09 11:56:58 +0300

    Toastr implemented in app

---
+++ client/src/store/actionTypes.js
@@ -0,0 +1,5 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const ROLE = 'ROLE';
+export const TOKEN = 'TOKEN';
+export const BAD_REQUEST = 'BAD_REQUEST';
--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,4 +1,8 @@
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
-export const ROLE = 'ROLE';
-export const TOKEN = 'TOKEN';
+import {
+  BAD_REQUEST, LOGIN, ROLE, TOKEN,
+} from './actionTypes';
+
+export const login = () => ({ type: LOGIN });
+export const setToken = (payload) => ({ type: TOKEN, payload });
+export const setRole = (payload) => ({ type: ROLE, payload });
+export const setBadRequest = (payload) => ({ type: BAD_REQUEST, payload });
