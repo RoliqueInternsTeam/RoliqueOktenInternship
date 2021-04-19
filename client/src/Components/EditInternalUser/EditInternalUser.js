71361c447f0bf5ae4283151ae080de152ec6e15e
0utfire <nataniel.rr@gmail.com>
2021-04-19 20:28:07 +0300

    add FormData converter lib

--- client/src/Components/EditInternalUser/EditInternalUser.js
+++ client/src/Components/EditInternalUser/EditInternalUser.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
+import { serialize } from 'object-to-formdata';
 import classes from './EditInternalUser.module.css';
 import Info from '../Elements/Icons/info.svg';
 import Input from '../Elements/Input/Input';
@@ -41,8 +42,10 @@ const EditInternalUser = (props) => {
   };
   const createHandler = async (event) => {
     event.preventDefault();
-    const formData = new FormData();
-    Object.keys(userInfo).forEach((key) => formData.append(key, userInfo[key]));
+
+    const formData = serialize(
+      userInfo,
+    );
 
     const request = {
       method: 'PUT',
