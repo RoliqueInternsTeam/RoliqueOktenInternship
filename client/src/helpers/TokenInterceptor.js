207b647192a4b399e7cfa07a88dbea80273bf7dc
Andrii <andriikosanyak4@gmail.com>
2021-05-13 13:52:32 +0300

    Redirect loop fix

--- client/src/helpers/TokenInterceptor.js
+++ client/src/helpers/TokenInterceptor.js
@@ -11,31 +11,32 @@ const axiosInstance = axios.create();
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const originalRequest = error.config;
+    try {
+      const originalRequest = error.config;
 
-    if (error.response.status === 401 && !refresh_token) {
+      if (error.response.status === 401 && !refresh_token) {
+        window.location.href = '/login';
+        return Promise.reject(error);
+      }
+
+      if (error.response.status === 401 && refresh_token) {
+        const response = await axios.post('http://localhost:5000/auth/refresh', {}, {
+          headers: {
+            AUTHORIZATION: refresh_token,
+          },
+        });
+
+        if (response.status === 200) {
+          const { access_token, refresh_token } = await response.data;
+          cookies.set('refresh_token', refresh_token);
+          store.dispatch(setToken(access_token));
+        }
+      }
+      return axiosInstance(originalRequest);
+    } catch (e) {
       window.location.href = '/login';
       return Promise.reject(error);
     }
-
-    if (error.response.status === 401 && refresh_token) {
-      const response = await axios.post('http://localhost:5000/auth/refresh', {}, {
-        headers: {
-          AUTHORIZATION: refresh_token,
-        },
-      });
-
-      if (response.status === 200) {
-        const { access_token, refresh_token } = await response.data;
-        cookies.set('refresh_token', refresh_token);
-        store.dispatch(setToken(access_token));
-      }
-
-      if (response.status !== 200 || response.message === 'Not token') {
-        window.location.href = '/login';
-      }
-    }
-    return axiosInstance(originalRequest);
   },
 );
 
