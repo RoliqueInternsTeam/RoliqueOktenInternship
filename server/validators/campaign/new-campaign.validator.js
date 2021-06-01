3c3cebbea493cfc14cd2446b3b76266a70ae02d0
Sem07 <Sem007@live.com>
2021-06-01 23:21:54 +0300

    add campaign validator

--- server/validators/campaign/new-campaign.validator.js
+++ server/validators/campaign/new-campaign.validator.js
@@ -10,17 +10,27 @@ module.exports = Joi.object({
     brand: Joi.string().trim().required(),
     teamLead: Joi.string().required(),
     campaignLogo: Joi.string().allow(null, ''),
-    clientDescription: Joi.string(),
-    internalNotes: Joi.string(),
+    clientDescription: Joi.string().allow(''),
+    internalNotes: Joi.string().allow(''),
     budgetsTargets: Joi.object().keys({
         totalBudget: Joi.number(),
-        budgets: Joi.object().keys({
-            youtubeUsername: Joi.string().allow(''),
-            youtubeFollowers: Joi.number().when(
-                'youtubeUsername', {
-                    is: '', then: Joi.any().valid(''), otherwise: Joi.number().required()
-                }
-            )
+        budgets: Joi.object({
+            influencerBudget: Joi.number(),
+            socialAdsMediaBudget: Joi.number(),
+            productionBudget: Joi.number(),
+            travelBudget: Joi.number(),
+            handlingFee: Joi.number(),
+            otherBudget: Joi.number(),
+        }).when('totalBudget', {
+            is: Joi.exist(),
+            then: Joi.object({
+                influencerBudget: Joi.allow('').required(),
+                socialAdsMediaBudget: Joi.allow('').required(),
+                productionBudget: Joi.allow('').required(),
+                travelBudget: Joi.allow('').required(),
+                handlingFee: Joi.allow('').required(),
+                otherBudget: Joi.allow('').required(),
+            })
         }),
 
     })
